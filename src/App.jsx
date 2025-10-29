6fc31eeb6a6c38bd7476ae46ea9de3aa3e4221c3
denali <denali.mcknight@my.uwi.edu>
2025-10-29 12:31:26 -0400

    Model Switcher added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
 import gsap from "gsap";
 import {ScrollTrigger} from "gsap/all";
+import {Shape} from "@react-three/drei";
+import Showcase from "./Showcase.jsx";
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -13,7 +15,7 @@ const App = () => {
             <NavBar/>
             <Hero/>
             <ProductViewer/>
-
+            <Showcase/>
         </div>
 
     )
